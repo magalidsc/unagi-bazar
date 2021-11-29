814178dca01b1be3f46d4e687214560844640930
Magali de Sousa <m.desousacandeias@gmail.com>
2021-11-29 20:28:32 -0300

    Incorporación de la función onAdd

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
+import "../components/itemCount.css"
 
 
+const ItemCount = ({ stock, initial }) => {
 
-const ItemCount = () => {
 
-    let stock = 10
-
-    const [count, setCounter] = useState(1)
+    const [count, setCounter] = useState(initial)
 
     const addProduct = () => {
         // Condicional para que solo se pueda sumar productos hasta llegar al límite del stock
         if (count < stock) {
             setCounter(count + 1);
         } else {
-            console.log("Se está exediendo el límite del stock")
+            console.log("Se está excediendo el límite del stock")
         }
     }
 
     const removeProduct = () => {
         // Restar una unidad por cada click. No puede restarse un número menor a 1
-        if (count > 1) {
+        if (count > 0) {
             setCounter(count - 1);
         } else {
             console.log("No se puede seleccionar menos de 1 producto")
@@ -27,11 +26,22 @@ const ItemCount = () => {
 
     }
 
+    const onAdd = () => {
+        if (count > 0) {
+            alert("Fueron agregados tus productos al carrito");
+        } else {
+            alert("Debes seleccionar al menos un producto")
+        }
+    }
+
     return (
-        <div>
-            <h3> {count} </h3>
-            <button onClick={removeProduct}> - </button>
-            <button onClick={addProduct}> + </button>
+        <div className="cardItem">
+            <h1>Nombre producto</h1>
+            <h5> Cantidad seleccionada: {count} </h5>
+            <button onClick={removeProduct} class="btn btn-secondary"> - </button>
+            <button onClick={addProduct} class="btn btn-secondary"> + </button>
+            <button onClick={onAdd} class="btn btn-outline-warning">Agregar al carrito</button>
+
         </div>
     )
 }
--- src/components/item-list-container/ItemListContainer.jsx
+++ src/components/item-list-container/ItemListContainer.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ItemCount from "../ItemCount.jsx";
+import "./style.css";
 
 const styleGreeting = {
-    margin: '2rem',
-    fontSize: '60px'
+    display: 'block',
+    margin: '8rem',
+    fontSize: '80px'
+
 }
 
 const ItemListContainer = ({ greeting }) => {
     return (
-        <div>
+        <div className="container">
             <h1 style={styleGreeting}>{greeting}</h1>
-            <ItemCount />
+            <ItemCount stock={10} initial={1} />
+            <ItemCount stock={8} initial={1} />
+            <ItemCount stock={6} initial={1} />
+            <ItemCount stock={8} initial={1} />
         </div>
     )
 }
